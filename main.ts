a9f7f8536f45a3542f73d5440cf1cff41421984b
Yohan Naftali <yohan.naftali@gmail.com>
2021-08-22 19:29:08 +0700

    function

--- main.ts
+++ main.ts
@@ -1,19 +1,22 @@
 input.onSound(DetectedSound.Loud, function () {
     basic.showIcon(IconNames.Heart)
 })
-let distance = 0
-rekabit.setRgbPixelColor(0, 0x007fff)
-rekabit.setRgbPixelColor(1, 0x00ff00)
-basic.forever(function () {
-    distance = 0
+function calculateDistance () {
     pins.digitalWritePin(DigitalPin.P0, 0)
     control.waitMicros(2)
     pins.digitalWritePin(DigitalPin.P0, 1)
     control.waitMicros(10)
     pins.digitalWritePin(DigitalPin.P0, 0)
     distance = Math.idiv(pins.pulseIn(DigitalPin.P1, PulseValue.High), 58)
-    basic.showNumber(distance)
-    if (distance < 30) {
+    return distance
+}
+let d = 0
+let distance = 0
+rekabit.setRgbPixelColor(0, 0x007fff)
+rekabit.setRgbPixelColor(1, 0x00ff00)
+basic.forever(function () {
+    d = calculateDistance()
+    if (d < 30) {
         soundExpression.giggle.playUntilDone()
     }
     basic.pause(100)
